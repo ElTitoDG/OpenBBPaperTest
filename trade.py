158fc10ee6c2ad29e29660b541c074b1d61c7095
Julián Hinojosa <julianhinojosa@MacBook-Air-de-Julian.local>
2022-08-20 23:56:35 +0200

    alpaca-py api doesn't work

--- trade.py
+++ trade.py
@@ -1,17 +1,20 @@
-from ast import If
 import alpaca_trade_api as api
 import config
-
-
 
 alpaca = api.REST(config.API_KEY, config.API_SECRET, config.BASE_URL)
 
 account = alpaca.get_account()
 asset = alpaca.get_asset("BTC/USD")
 clock = alpaca.get_clock()
+position = alpaca.get_position("ETHUSD")
+
+print("\n")
 print(account.cash)
-print(asset)
-if (clock.is_open == True):
+print(asset.symbol, asset.exchange)
+print(position.unrealized_pl)
+print("\n")
+
+if clock.is_open is True:
     print("Market is open")
 else:
     print("Market is closed")
---
+++ trade2.py
@@ -0,0 +1,8 @@
+from alpaca.trading.client import TradingClient
+import config
+
+# paper=True enables paper trading
+trading_client = TradingClient(config.API_KEY, config.API_SECRET, paper=True)
+
+#account = trading_client.get_account()
+account = trading_client.get_account()
