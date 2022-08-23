8f120b149ad76b7c611a43e16338a72b2fabbfd3
Julián Hinojosa <julianhinojosa@MacBook-Air-de-Julian.local>
2022-08-23 14:08:37 +0200

    Finally alpaca-py works

--- trade2.py
+++ trade2.py
@@ -7,4 +7,5 @@ trading_client = TradingClient(config.API_KEY, config.API_SECRET, paper=True)
 #account = trading_client.get_account()
 account = trading_client.get_account()
 
-print(account)
+print("Current cash in account:", account.cash, "$")
+print(account.json)
