a102e34d988d939a08e4ae1c025b866d98b4e2ac
Julian Hinojosa García <julianhinojosa@MacBook-Pro-de-Julian.local>
2022-08-29 22:47:18 +0200

    testting

--- trade2.py
+++ trade2.py
@@ -4,8 +4,10 @@ import config
 # paper=True enables paper trading
 trading_client = TradingClient(config.API_KEY, config.API_SECRET, paper=True)
 
-#account = trading_client.get_account()
 account = trading_client.get_account()
+open_pos = trading_client.get_open_position("BTCUSD")
+all_open_pos = trading_client.get_all_positions()
 
 print("Current cash in account:", account.cash, "$")
-print(account.json)
+print("Open Position: ", open_pos.symbol)
+print("Quantity: ", open_pos.qty)
